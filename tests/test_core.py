45a0a84b53a3817c72b91f14352d237366a7fbd8
Sam Clegg <sbc@chromium.org>
2020-12-21 04:57:43 -0800

    Compile all system libraries with `-g`
    As an alternative to shipping unoptimized debug copies of all
    libraries just add debug into the optimized libraries.  Release
    build are linked with `--strip-debug` so should not be effected.
    
    Fixes: #12060

--- tests/test_core.py
+++ tests/test_core.py
@@ -6948,7 +6948,7 @@ someweirdtext
       # the file attribute is optional, but if it is present it needs to refer
       # the output file.
       self.assertPathsIdentical(map_referent, data['file'])
-    assert len(data['sources']) == 1, data['sources']
+    self.assertGreater(len(data['sources']), 1)
     self.assertPathsIdentical('src.cpp', data['sources'][0])
     if hasattr(data, 'sourcesContent'):
       # the sourcesContent attribute is optional, but if it is present it
@@ -6962,14 +6962,14 @@ someweirdtext
       mappings = encode_utf8(mappings)
     seen_lines = set()
     for m in mappings:
-      self.assertPathsIdentical('src.cpp', m['source'])
-      seen_lines.add(m['originalLine'])
+      if m['source'] == 'src.cpp':
+        seen_lines.add(m['originalLine'])
     # ensure that all the 'meaningful' lines in the original code get mapped
     # when optimizing, the binaryen optimizer may remove some of them (by inlining, etc.)
     if is_optimizing(self.emcc_args):
-      assert seen_lines.issuperset([11, 12]), seen_lines
+      self.assertTrue(seen_lines.issuperset([11, 12]), seen_lines)
     else:
-      assert seen_lines.issuperset([6, 7, 11, 12]), seen_lines
+      self.assertTrue(seen_lines.issuperset([6, 7, 11, 12]), seen_lines)
 
   @no_wasm2js('TODO: source maps in wasm2js')
   def test_dwarf(self):
@@ -7032,7 +7032,14 @@ someweirdtext
     # ------------------ ------ ------ ------ --- ------------- -------------
     # 0x000000000000000b      5      0      3   0             0  is_stmt
     src_to_addr = {}
+    found_src_cpp = False
     for line in sections['.debug_line'].splitlines():
+      if 'name: "src.cpp"' in line:
+        found_src_cpp = True
+      if not found_src_cpp:
+        continue
+      if 'debug_line' in line:
+        break
       if line.startswith('0x'):
         while '  ' in line:
           line = line.replace('  ', ' ')
@@ -7047,7 +7054,8 @@ someweirdtext
 
     def get_dwarf_addr(line, col):
       addrs = src_to_addr[(line, col)]
-      assert len(addrs) == 1, 'we assume the simple calls have one address'
+      # we assume the simple calls have one address
+      self.assertEqual(len(addrs), 1)
       return int(addrs[0], 0)
 
     # the lines must appear in sequence (as calls to JS, the optimizer cannot
